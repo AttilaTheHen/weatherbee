b65ded7816b43aafa8afe8864348f28cb51b3786
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-11-14 21:16:09 -0800

    Make function for grabbing morning data from returned api data

--- js/app.js
+++ js/app.js
@@ -2,11 +2,19 @@ import getWeather from '../services/weather-api.js';
 import API_KEY from '../config.js';
 
 const searchForm = document.getElementById('search-form');
+let weatherData;
 
 searchForm.addEventListener('submit', () => {
     event.preventDefault();
-    
+
     const zip = document.getElementById('search').value;
-    getWeather(zip, API_KEY)
-        .then(data => console.log(data));
+    morningTemp(zip);
 });
+
+function morningTemp(zip) {
+    getWeather(zip, API_KEY)
+        .then(data => {
+            weatherData = data.list.filter(time => new Date(time.dt_txt).toLocaleTimeString() === '6:00:00 AM');
+            console.log(weatherData);
+        });
+}
